5e88cad007d917d33cbe6785fc8f54f58f4abb17
itimdad <iamfirdausi01@gmail.com>
2026-01-19 15:43:34 +0530

    added repository classes

--- src/main/java/com/imdad/entity/UserEntity.java
+++ src/main/java/com/imdad/entity/UserEntity.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +24,7 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "IES_USERS")
 public class UserEntity {
 	
 	@Id
@@ -33,6 +36,7 @@ public class UserEntity {
 	private String phno;
 	private String gender;
 	private LocalDate dob;
+	@Column(name = "ssn_num")
 	private String userSSN;
 	private String activeSwitch = "Y";
 	private String accountStatus = "LOCKED";
