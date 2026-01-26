caea463a698ca0bde61237d3ff78fb3e498ce3db
Imdad Firdausi <iamfirdausi01@gmail.com>
2026-01-27 01:59:10 +0530

    implement changes in planservice

--- src/main/java/com/imdad/binding/DashboardCards.java
+++ src/main/java/com/imdad/binding/DashboardCards.java
@@ -1,5 +1,18 @@
 package com.imdad.binding;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
 public class DashboardCards {
+
+    private Long totalPlans;
+    private Long approvedCitizens;
+    private Long deniedCitizens;
+    private Long totalBenifitGiven;
+
+
+
 
 }
--- src/main/java/com/imdad/service/UserServiceImpl.java
+++ src/main/java/com/imdad/service/UserServiceImpl.java
@@ -3,15 +3,24 @@ package com.imdad.service;
 import com.imdad.binding.DashboardCards;
 import com.imdad.binding.LoginForm;
 import com.imdad.entity.UserEntity;
+import com.imdad.repository.PlanRepository;
 import com.imdad.repository.UserRepository;
+import com.imdad.utils.EmailUtils;
 
 public class UserServiceImpl implements UserService {
 
 	UserRepository userRepository;
+	EmailUtils emailUtils;
+	PlanRepository planRepository;
 
-	UserServiceImpl(UserRepository userRepository){
+	UserServiceImpl(UserRepository userRepository,
+					EmailUtils emailUtils,
+					PlanRepository planRepository){
 		this.userRepository = userRepository;
+		this.emailUtils = emailUtils;
+		this.planRepository = planRepository;
 	}
+
 	@Override
 	public String login(LoginForm form) {
 
@@ -20,20 +29,45 @@ public class UserServiceImpl implements UserService {
 //
 //		if(user == null) {
 //
-//		}
+//		}s
 		return "";
 	}
 
 	@Override
 	public boolean recoverPwd(String email) {
-		return false;
+
+		UserEntity user = userRepository.findByEmail(email);
+
+		if(user == null) {
+			return false;
+		}
+		String body = "";
+		String subject = "";
+		String to = email;
+
+		try {
+			emailUtils.sendMail(subject, to, body);
+		}
+		catch(Exception e) {
+			return false;
+		}
+
+		return true;
 	}
 
 
 	@Override
 	public DashboardCards getDashboardData() {
 		// TODO Auto-generated method stub
-		return null;
+		long count = planRepository.count();
+
+		DashboardCards dashboardCards = new DashboardCards();
+		dashboardCards.setTotalPlans(count);
+		dashboardCards.setTotalBenifitGiven(25l);
+		dashboardCards.setDeniedCitizens(33l);
+		dashboardCards.setApprovedCitizens(66l);
+
+		return dashboardCards;
 	}
 
 }
